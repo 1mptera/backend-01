a322a7822960d7af7204d82b1be4469ca8c89e34
Inwoo Hwang <hsjkdss228@naver.com>
2022-06-04 00:58:10 +0900

    220603-spiralMatrix-hiw (2)
    SpiralMatrix.java 소스코드 작성을 마쳤습니다.
    소스코드 구상에 참고한 자료 이미지 파일을 첨부했습니다.

--- 220603-spiralmatrix-hiw/SpiralMatrix.java
+++ 220603-spiralmatrix-hiw/SpiralMatrix.java
@@ -19,9 +19,52 @@ public class SpiralMatrix {
 
         //처리
         int[][] spiralArray = new int[length][length];
+        int starts = 0;
+        int ends = length - 1;
 
+        int puttingNumber = 0;
 
+        while (starts <= ends) {
+            //Left to Right
+            for (int i = starts; i <= ends; i += 1) {
+                spiralArray[starts][i] = puttingNumber;
+                puttingNumber += 1;
+            }
+
+            //Up to Down
+            if (starts + 1 <= ends) {
+                for (int i = starts + 1; i <= ends; i += 1) {
+                    spiralArray[i][ends] = puttingNumber;
+                    puttingNumber += 1;
+                }
+            }
+
+            //Right to Left
+            if (ends - 1 >= starts) {
+                for (int i = ends - 1; i >= starts; i -= 1) {
+                    spiralArray[ends][i] = puttingNumber;
+                    puttingNumber += 1;
+                }
+            }
+
+            //Down to Up
+            if (ends - 1 >= starts + 1) {
+                for (int i = ends - 1; i >= starts + 1; i -= 1) {
+                    spiralArray[i][starts] = puttingNumber;
+                    puttingNumber += 1;
+                }
+            }
+
+            starts += 1;
+            ends -= 1;
+        }
 
         //출력
+        for (int i = 0; i < spiralArray.length; i += 1) {
+            for (int j = 0; j < spiralArray.length; j += 1) {
+                System.out.print(spiralArray[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
