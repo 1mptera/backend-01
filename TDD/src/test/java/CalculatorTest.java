91a76cada6e6c3322b3a341cfdfd755b78e7b909
myunghoonkim <xcv8826@gmail.com>
2022-06-07 16:42:50 +0900

    220607-calculator-jsh-kmh
    first TDD try with SeongHwan

--- TDD/src/test/java/CalculatorTest.java
+++ TDD/src/test/java/CalculatorTest.java
@@ -1,4 +1,28 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+  Calculator calculator = new Calculator();
+
+  @Test
+  void simpleTest() {
+    assertEquals(1, calculator.compute("0 + 1"));
+    assertEquals(2, calculator.compute("0 + 2"));
+  }
+  @Test
+  void minus() {
+    assertEquals(1, calculator.compute("2 - 1"));
+    assertEquals(2, calculator.compute("4 - 2"));
+  }
+  @Test
+  void times() {
+    assertEquals(1, calculator.compute("1 * 1"));
+    assertEquals(2, calculator.compute("2 * 1"));
+  }
+  @Test
+  void divide() {
+    assertEquals(1, calculator.compute("2 / 2"));
+    assertEquals(2, calculator.compute("4 / 2"));
+  }
 }
