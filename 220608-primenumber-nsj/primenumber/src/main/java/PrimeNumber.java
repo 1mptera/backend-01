ce81cab293e84636b4c9f677354a87ef89f24795
seungjjun <il1uso@naver.com>
2022-06-08 09:18:45 +0900

    "소수 찾기" 문제 풀기 위한 기초 세팅

---
+++ 220608-primenumber-nsj/primenumber/src/main/java/PrimeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args){
+        // 준비
+        Scanner scanner = new Scanner(System.in);
+
+        // 입력
+        System.out.print("n :");
+        int naturalNumber = scanner.nextInt();
+
+        // 처리
+        int primeNumber = process(naturalNumber);
+
+        // 출력
+
+    }
+
+    private static int process(int naturalNumber) {
+        
+        return 0;
+    }
+}
