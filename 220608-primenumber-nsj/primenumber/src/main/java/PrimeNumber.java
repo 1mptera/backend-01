8197f421cf7211eceb56194a8e999c99182ca805
seungjjun <il1uso@naver.com>
2022-06-08 09:36:16 +0900

    입력받은 자연수가 소수인지 아닌지 판별하는 테스트 코드 작성

--- 220608-primenumber-nsj/primenumber/src/main/java/PrimeNumber.java
+++ 220608-primenumber-nsj/primenumber/src/main/java/PrimeNumber.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args){
+        PrimeNumber application = new PrimeNumber();
+        application.run();
+    }
+
+    private void run() {
         // 준비
         Scanner scanner = new Scanner(System.in);
 
@@ -10,14 +15,20 @@ public class PrimeNumber {
         int naturalNumber = scanner.nextInt();
 
         // 처리
-        int primeNumber = process(naturalNumber);
+        String primeNumber = process(naturalNumber);
 
         // 출력
-
+        System.out.println(primeNumber);
     }
 
-    private static int process(int naturalNumber) {
-        
-        return 0;
+    public String process(int naturalNumber) {
+
+        for (int i = 2; i < naturalNumber; i += 1) {
+            if(naturalNumber % i == 0) {
+                return "소수 아님";
+            }
+        }
+
+        return "소수";
     }
 }
--- 220608-primenumber-nsj/primenumber/src/test/java/PrimeNumberTest.java
+++ 220608-primenumber-nsj/primenumber/src/test/java/PrimeNumberTest.java
@@ -6,6 +6,11 @@ class PrimeNumberTest {
     @Test
     void process() {
         PrimeNumber primeNumber = new PrimeNumber();
+
+        assertEquals("소수 아님", primeNumber.process(1));
+        assertEquals("소수 아님", primeNumber.process(200));
+        assertEquals("소수", primeNumber.process(17));
+        assertEquals("소수", primeNumber.process(7741));
     }
 
 }
