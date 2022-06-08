21d80a37e4eb498717d13dd02c02c8ca8faaf92d
Jeon Minji <mjjeon2645@gmail.com>
2022-06-08 10:36:43 +0900

    [220608-helloworld-jmj]
    1. build.gradle 파일 내 plugins 영역에 application을 추가하여 gradlew tasks를 확장하였음
    
    2. run 메소드를 별도로 생성하여 "Hello, world!" 문구를 띄움

---
+++ 220608-helloworld-jmj/demo/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.run();
+    }
+
+    public void run() {
+        System.out.println("Hello, world!");
+    }
+}
