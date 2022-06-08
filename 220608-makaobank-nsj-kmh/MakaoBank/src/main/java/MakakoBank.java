0d883f8eaf0eaf4aca3b97415fe2f8e780018de3
seungjjun <il1uso@naver.com>
2022-06-08 13:29:07 +0900

    MakaoBank process set-up
     - build.gradle 파일에 application과 mainClassName을 설정
       set applicaion and mainClassName on bulid.gradle file.
    
     - MakaoBank 클래스 준비 -> 입력 -> 처리 -> 출력 각 단계에 대한 명령어 준비
     - get commands set for ready,input,process,output
    
     - 계좌거래정보 csv 파일 생성
     - make csv file for transactions

---
+++ 220608-makaobank-nsj-kmh/MakaoBank/src/main/java/MakakoBank.java
@@ -0,0 +1,22 @@
+import javax.security.auth.login.AccountException;
+
+public class MakakoBank {
+    public static void main(String[] args) {
+        MakakoBank application = new MakakoBank();
+        application.run();
+    }
+
+    private void run() {
+        // 준비
+        Account account = new Account();
+
+        // 입력
+        List<Transaction> transactions = loadTransaction();
+
+        // 처리
+        List<transactionResult> transactionResults = account.process(transactions);
+
+        // 출력
+        saveTransactionResult(transactionResults);
+    }
+}
