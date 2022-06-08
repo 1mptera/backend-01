e228e0c380a87129867abcb35c44e82fae63f189
Jeon Minji <mjjeon2645@gmail.com>
2022-06-08 09:42:51 +0900

    [220608-primenumber-jmj]
    primenumber 클래스의 입력-처리-출력 구조를 메소드화하여 가시성의 편의를 높임
    테스트파일을 별도로 생성하여 숫자 1을 입력하였을 때, 원하는 결과값이 출력되도록 설정함

--- 220608-primenumber-jmj/untitled/src/main/java/PrimeNumber.java
+++ 220608-primenumber-jmj/untitled/src/main/java/PrimeNumber.java
@@ -1,23 +1,41 @@
 import java.util.Scanner;
 
 public class PrimeNumber {
+    private String result = "소수";
+    private int inputNumber;
+
     public static void main(String[] args) {
         PrimeNumber application = new PrimeNumber();
         application.run();
     }
 
     public void run() {
-        // 준비
-        Scanner scanner = new Scanner(System.in);
-
         // 입력
-        System.out.print("n: ");
-        int inputNumber = scanner.nextInt();
+        int inputNumber = getInputNumber();
 
         // 처리
-        String result = "";
+        processInputNumber(inputNumber);
 
         // 출력
         System.out.println(result);
+    }
+
+    public int getInputNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n: ");
+        inputNumber = scanner.nextInt();
+        return inputNumber;
+    }
+
+    public String processInputNumber(int inputNumber) {
+        int quotient = 0;
+        int remainder = 0;
+
+        if (inputNumber == 1) {
+            result = "소수 아님";
+            return result;
+        }
+
+        return result;
     }
 }
---
+++ 220608-primenumber-jmj/untitled/src/test/java/PrimeNumberTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeNumberTest {
+
+    @Test
+    void inputNumber1() {
+        PrimeNumber primeNumber = new PrimeNumber();
+        assertEquals("소수 아님", primeNumber.processInputNumber(1));
+    }
+}
