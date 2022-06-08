649abdd7142dcaa8dc0bb1c274a2d98ba3a02b72
Jeon Minji <mjjeon2645@gmail.com>
2022-06-08 09:22:35 +0900

    [220608-primenumber-jmj]
    테스트코드 작성 전, 소수를 찾기 위한 메인 클래스의 기본 구조를 빌드업 함

---
+++ 220608-primenumber-jmj/untitled/src/main/java/PrimeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        PrimeNumber application = new PrimeNumber();
+        application.run();
+    }
+
+    public void run() {
+        // 준비
+        Scanner scanner = new Scanner(System.in);
+
+        // 입력
+        System.out.print("n: ");
+        int inputNumber = scanner.nextInt();
+
+        // 처리
+        String result = "";
+
+        // 출력
+        System.out.println(result);
+    }
+}
