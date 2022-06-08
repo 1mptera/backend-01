bc573ca656ffa67ae132cb36f206e8bc953f63eb
seungjjun <il1uso@naver.com>
2022-06-08 11:38:33 +0900

    계산기를 만들기 위한 준비 작업
     - 준비 -> 입력 -> 처리 -> 출력 각 단계에 필요한 코드 작성
    
     - 입력받은 식을 처리하기 위한 "compute" 메서드 작성

--- 220608-calculator-nsj/Calculator/src/main/java/Calculator.java
+++ 220608-calculator-nsj/Calculator/src/main/java/Calculator.java
@@ -1,2 +1,34 @@
+// 입력은 식(Expression) -> (숫자 연산자 숫자) 형식으로 받는다.
+// expression -> (1 + 1 => right) (1+1 => wrong)
+// 입력받은 식은 "compute" 메서드로 계산을 한 뒤 출력한다.
+
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Calculator application = new Calculator();
+        application.run();
+    }
+
+    private void run() {
+        // 준비
+        Scanner scanner = new Scanner(System.in);
+
+        // 입력
+        System.out.println("Input Expression : ");
+
+        String expression = scanner.nextLine();
+
+        // 처리
+        int result = compute(expression);
+
+        // 출력
+        System.out.println(result);
+    }
+
+    private int compute(String expression) {
+
+        return 0;
+    }
 }
+
