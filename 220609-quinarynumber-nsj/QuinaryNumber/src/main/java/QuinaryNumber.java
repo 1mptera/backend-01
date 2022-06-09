269beaf9e14741ae7bdbdc514173baa390f1c25c
seungjjun <il1uso@naver.com>
2022-06-09 09:55:26 +0900

    build.gradle 기본 설정과 문제 요구사항을 파악했다

---
+++ 220609-quinarynumber-nsj/QuinaryNumber/src/main/java/QuinaryNumber.java
@@ -0,0 +1,22 @@
+// 5진수 만들기
+// - 5진수로 변환할 자연수를 입력받는다
+// - 입력받은 수를 5로 나눌수 없을 때 까지 반복해서 나눈다.
+// - 그 나머지들을 거꾸로 읽은 수가 5진수로 변환한 수 이다.
+
+public class QuinaryNumber {
+    public static void main(String[] args) {
+        QuinaryNumber application = new QuinaryNumber();
+        application.run();
+    }
+
+    public void run() {
+        // 준비
+
+        // 입력
+
+        // 처리
+        
+        // 출력
+
+    }
+}
