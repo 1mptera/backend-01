8a41e30ff5fab550c6f8b36a9ef9e4a0392c9204
seungjjun <il1uso@naver.com>
2022-06-09 10:33:43 +0900

    build.gradle 파일에서 plugin에 application을 추가하고 mainClassName을 설정했다.

---
+++ 220609-helloworld-nsj/HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        HelloWorld application = new HelloWorld();
+        application.run();
+    }
+
+    public void run() {
+        
+    }
+}
