a841835d42385e4b50645f029e8cb324af1cbef4
seungjjun <il1uso@naver.com>
2022-06-09 11:32:45 +0900

    Calculator을 만들기 위해서 입력 -> 처리 -> 출력 각 단계별로 나누고 계산을 해주는 compute 메서드를 생성했다.

--- 220609-calculator-nsj/Calculator/src/main/java/Calculator.java
+++ 220609-calculator-nsj/Calculator/src/main/java/Calculator.java
@@ -1,3 +1,33 @@
-public class Calculator {
+// Calculator
+// - 계산할 식을 입력 받는다.
+// - 식은 (숫자 연산자 숫자) 형태로 입력한다.
+// - 입력받은 식을 계산해주는 compute 메서드를 작성한다.
 
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator application = new Calculator();
+        application.run();
+    }
+
+    private void run() {
+        // 입력
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input Expression : ");
+
+        String expression = scanner.nextLine();
+
+        // 처리
+        int result = compute(expression);
+
+        // 출력
+        System.out.println("Result : " + result);
+    }
+
+    private int compute(String expression) {
+
+        return 0;
+    }
 }
