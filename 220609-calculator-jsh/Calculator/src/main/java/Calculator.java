02aff48e7b0d6417c6139461f24f21040c183866
sunghwan22 <tidls3144@naver.com>
2022-06-09 11:32:00 +0900

    calculator 입력 처리 출력 에대한 구성을 완료

---
+++ 220609-calculator-jsh/Calculator/src/main/java/Calculator.java
@@ -0,0 +1,29 @@
+//main -> run
+// 입력 처리 출력
+// 입력    식(expression) ex 1 + 1
+//
+
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args){
+        Calculator application = new Calculator();
+        application.run();
+    }
+
+    public void run() {
+        //준비
+        Scanner scanner = new Scanner(System.in);
+        //입력
+        System.out.print("식을 입력해 주세요: ");
+        String expression = scanner.nextLine();
+
+        //처리
+        int result = compute(expression);
+
+        //출력
+        System.out.print(result);
+
+    }
+}
