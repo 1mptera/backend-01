c886880d8f6fcfea4d630006978e45dc09e36312
Jeon Minji <mjjeon2645@gmail.com>
2022-06-10 10:33:58 +0900

    220609-makaobank-jmj
    파일 읽고 쓰기 2강 중간부분까지 저장

--- 220609-makaobank-jmj/demo/src/main/java/models/Account.java
+++ 220609-makaobank-jmj/demo/src/main/java/models/Account.java
@@ -1,5 +1,12 @@
 package models;
 
+import java.util.List;
+
 public class Account {
   // TODO: 잔액 관리
+
+  public List<TransactionResult> process(List<Transaction> transactions) {
+    return null;
+  }
+
 }
--- 220609-makaobank-jmj/demo/src/test/java/models/TransactionTest.java
+++ 220609-makaobank-jmj/demo/src/test/java/models/TransactionTest.java
@@ -1,5 +1,5 @@
-// 1. 거래정보 만들기
-// 2. 거래정보 비교하기
+// 1. 거래정보 만들기 완료.
+// 2. 거래정보 비교하기 완료. 테스트 결과 내용이 잘 보이도록!. 완성
 // 3. 잔액 변동
 // 4. 잔액 변하게 한 결과 도출?
 
@@ -23,4 +23,10 @@ class TransactionTest {
     assertEquals(transaction1, transaction2);
   }
 
+  @Test
+  void string() {
+    Transaction transaction = new Transaction("입금", 1000);
+
+    assertEquals("Transaction(입금: 1000)", transaction.toString());
+  }
 }
