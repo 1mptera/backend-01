8c6bea40b3db50c80b738f09e531538e14a56636
seungjjun <il1uso@naver.com>
2022-06-12 15:15:27 +0900

    build.gradle 파일에서 plugin에 application 추가, mainClassName을 spiralmatrix로 설정

---
+++ 220612-spiralmatrix-nsj/spiralMatrix/src/main/java/SpiralMatrix.java
@@ -0,0 +1,10 @@
+public class SpiralMatrix {
+  public static void main(String[] args) {
+    SpiralMatrix application = new SpiralMatrix();
+    application.run();
+  }
+
+  public void run() {
+    
+  }
+}
