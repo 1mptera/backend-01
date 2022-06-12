fd0d533024a065b1cdfa3834a43d6f2009c311f6
seungjjun <il1uso@naver.com>
2022-06-12 17:11:12 +0900

    220612-spiralMatrix-nsj
    spiralMatrix

--- 220612-spiralmatrix-nsj/spiralMatrix/src/main/java/SpiralMatrix.java
+++ 220612-spiralmatrix-nsj/spiralMatrix/src/main/java/SpiralMatrix.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SpiralMatrix {
   public static void main(String[] args) {
     SpiralMatrix application = new SpiralMatrix();
@@ -5,6 +7,45 @@ public class SpiralMatrix {
   }
 
   public void run() {
-    
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("배열의 한변 길이 : ");
+    int oneSideLength = scanner.nextInt();
+
+    int[][] matrix = new int[oneSideLength][oneSideLength];
+
+    int row = 0;
+    int column = 0;
+    int addNumber = 0;
+    int conversion = 1;
+    int minusLength = oneSideLength;
+    double lastNumber = Math.pow(oneSideLength, 2) - 1;
+
+
+    while (lastNumber != addNumber) {
+      for(int i = 0; i < minusLength; i += 1) {
+        row += conversion;
+        matrix[column][row] = addNumber;
+        addNumber += 1;
+      }
+
+      minusLength -= 1;
+
+      for(int i = 0; i < minusLength; i += 1) {
+        column += conversion;
+        matrix[column][row] = addNumber;
+        addNumber += 1;
+      }
+
+      conversion *= -1;
+    }
+
+    for (int i = 0; i < oneSideLength; i += 1) {
+      for (int j = 0; j < oneSideLength; j += 1) {
+        System.out.print(matrix[i][j]);
+        System.out.print("\t");
+      }
+    }
   }
 }
+
