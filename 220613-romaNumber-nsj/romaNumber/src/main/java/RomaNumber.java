9a3230f24baff594cd9be4194e1aaab3fd4acbf1
seungjjun <il1uso@naver.com>
2022-06-13 09:53:41 +0900

    작은 숫자가 큰 숫자보다 먼저 오는 경우에 해당 숫자를 빼는 과정까지 처리

--- 220613-romaNumber-nsj/romaNumber/src/main/java/RomaNumber.java
+++ 220613-romaNumber-nsj/romaNumber/src/main/java/RomaNumber.java
@@ -1,36 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RomaNumber {
+  private int eachArabiaNumber = 0;
+  private int sumArabiaNumber = 0;
+
   public static void main(String[] args) {
     RomaNumber application = new RomaNumber();
     application.run();
   }
 
   public void run() {
-    // 준비
-    Scanner scanner = new Scanner(System.in);
-
     // 입력
-    System.out.print("로마 숫자 : ");
-    String romaNumber = scanner.nextLine();
+    String romaNumber = inputRomaNumber();
 
     // 처리
     int arabiaNumber = process(romaNumber);
 
     // 출력
-    System.out.print("아라비아 숫자 : " + arabiaNumber);
+    displayArabiaNumber(arabiaNumber);
+  }
+
+  public String inputRomaNumber() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("로마 숫자 : ");
+    String romaNumber = scanner.nextLine();
+    return romaNumber;
   }
 
   public int process(String romaNumber) {
-    if(romaNumber.equals("I")) {
-      return 1;
+    String[] romaNumberList = romaNumber.split("");
+
+    List<Integer> arabiaNumberList = new ArrayList<>();
+
+    for (int i = 0; i < romaNumberList.length; i += 1) {
+      if (romaNumberList[i].equals("I")) {
+        eachArabiaNumber = 1;
+      }
+      if (romaNumberList[i].equals("V")) {
+        eachArabiaNumber = 5;
+      }
+      if (romaNumberList[i].equals("X")) {
+        eachArabiaNumber = 10;
+      }
+      if (romaNumberList[i].equals("L")) {
+        eachArabiaNumber = 50;
+      }
+      if (romaNumberList[i].equals("C")) {
+        eachArabiaNumber = 100;
+      }
+      if (romaNumberList[i].equals("D")) {
+        eachArabiaNumber = 500;
+      }
+      if (romaNumberList[i].equals("M")) {
+        eachArabiaNumber = 1000;
+      }
+
+      sumArabiaNumber += eachArabiaNumber;
+      arabiaNumberList.add(eachArabiaNumber);
     }
 
-    if(romaNumber.equals("L")) {
-      return 50;
+    for (int i = 0; i < arabiaNumberList.size() - 1; i += 1) {
+      if (arabiaNumberList.get(i) < arabiaNumberList.get(i + 1)) {
+        sumArabiaNumber -= arabiaNumberList.get(i) * 2;
+      }
     }
 
-    return 0;
+    return sumArabiaNumber;
+  }
+
+  public void displayArabiaNumber(int arabiaNumber) {
+    System.out.print("아라비아 숫자 : " + arabiaNumber);
   }
 }
 
