a0d2d4f3f010f009317a4b161c045f29b40a95c6
seungjjun <il1uso@naver.com>
2022-06-13 09:12:24 +0900

    build.gradle 기본 설정

---
+++ 220613-romaNumber-nsj/romaNumber/src/main/java/RomaNumber.java
@@ -0,0 +1,10 @@
+public class RomaNumber {
+  public static void main(String[] args) {
+    RomaNumber application = new RomaNumber();
+    application.run();
+  }
+
+  public void run() {
+    
+  }
+}
