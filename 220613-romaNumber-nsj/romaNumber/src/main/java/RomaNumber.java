7cb179dc2b7091501f8828f3471a38023b4b5c7c
seungjjun <il1uso@naver.com>
2022-06-13 09:18:38 +0900

    로마 숫자를 아라비아 숫자로 변환하는 process 메서드에 대한 테스트코드 작성

--- 220613-romaNumber-nsj/romaNumber/src/main/java/RomaNumber.java
+++ 220613-romaNumber-nsj/romaNumber/src/main/java/RomaNumber.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class RomaNumber {
   public static void main(String[] args) {
     RomaNumber application = new RomaNumber();
@@ -5,6 +7,23 @@ public class RomaNumber {
   }
 
   public void run() {
-    
+    // 준비
+    Scanner scanner = new Scanner(System.in);
+
+    // 입력
+    System.out.print("로마 숫자 : ");
+    String romaNumber = scanner.nextLine();
+
+    // 처리
+    int arabiaNumber = process(romaNumber);
+
+    // 출력
+    System.out.print("아라비아 숫자 : " + arabiaNumber);
+  }
+
+  public int process(String romaNumber) {
+
+    return 0;
   }
 }
+
---
+++ 220613-romaNumber-nsj/romaNumber/src/test/java/RomaNumberTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomaNumberTest {
+  @Test
+  void conversion() {
+    RomaNumber romaNumber = new RomaNumber();
+
+    assertEquals(1, romaNumber.process("I"));
+    assertEquals(50, romaNumber.process("L"));
+  }
+
+}
