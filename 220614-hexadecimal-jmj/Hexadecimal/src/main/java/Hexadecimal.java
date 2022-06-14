df9d8a86bec1c054e1c32f46855f1edd9fd78d0f
Jeon Minji <mjjeon2645@gmail.com>
2022-06-14 09:12:30 +0900

    220614-Hexadecimal-전민지
    1. static main, non-static run 메소드를 선언하였음
    2. run 메소드에 준비-입력-처리-출력 단계를 나누어 프로그램이 작동하는 가장 최소한의 소스코드 작성

---
+++ 220614-hexadecimal-jmj/Hexadecimal/src/main/java/Hexadecimal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Hexadecimal {
+  public static void main(String[] args) {
+    Hexadecimal application = new Hexadecimal();
+    application.run();
+  }
+
+  public void run() {
+    // 준비
+    Scanner scanner = new Scanner(System.in);
+    String symbolTable = "ABCDEF";
+
+    // 입력
+    System.out.print("십진수를 입력해주세요: ");
+    int inputNumber = scanner.nextInt();
+
+    // 처리
+    String hexadecimal = "";
+
+    // 출력
+    System.out.println("16진수: " + hexadecimal);
+  }
+}
