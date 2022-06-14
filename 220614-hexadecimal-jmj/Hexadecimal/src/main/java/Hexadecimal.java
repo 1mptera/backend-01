86f68ee7939c25bef421875575f6034692fe46bd
Jeon Minji <mjjeon2645@gmail.com>
2022-06-14 12:54:43 +0900

    예시에 있는 모든 수에 대한 테스트 설계(현재 질문 중)
    실제 프로그램 구동 시 모든 답이 정확하게 나오나, 테스트만 돌아가지 않음

--- 220614-hexadecimal-jmj/Hexadecimal/src/main/java/Hexadecimal.java
+++ 220614-hexadecimal-jmj/Hexadecimal/src/main/java/Hexadecimal.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Hexadecimal {
-  private String symbolTable = "ABCDEF";
-  private String hexadecimal;
+  private String hexadecimal = "";
+  private String tempHexadecimal = "";
 
   public static void main(String[] args) {
     Hexadecimal application = new Hexadecimal();
@@ -18,25 +18,37 @@ public class Hexadecimal {
     int inputNumber = scanner.nextInt();
 
     // 처리
-    hexadecimal = "";
+    hexadecimal = compute(inputNumber);
 
     // 출력
     System.out.println("16진수: " + hexadecimal);
   }
 
   public String compute(int inputNumber) {
-    if (inputNumber < 10) {
-      return Integer.toString(inputNumber);
-    }
+    String symbolTable = "0123456789ABCDEF";
 
-    if (inputNumber >= 10 && inputNumber < 16) {
-      hexadecimal = String.valueOf(symbolTable.charAt(inputNumber - 10));
-      return hexadecimal;
+    if (inputNumber < 16) {
+      return String.valueOf(symbolTable.charAt(inputNumber));
     }
 
     if (inputNumber >= 16) {
-      return "5C";
+      int quotient = 1;
+      int remainder = 0;
+
+      for (int i = 0; 0 < quotient; i += 1) {
+        quotient = inputNumber / 16;
+        remainder = inputNumber % 16;
+
+        tempHexadecimal += String.valueOf(symbolTable.charAt(remainder));
+
+        inputNumber = quotient;
+      }
+
+      for (int i = tempHexadecimal.length(); i > 0; i -= 1) {
+        hexadecimal += String.valueOf(tempHexadecimal.charAt(i - 1));
+      }
+      return hexadecimal;
     }
-    return "환산이 불가능합니다.";
+    return "범위에 맞는 값을 입력해주세요(자연수)";
   }
 }
--- 220614-hexadecimal-jmj/Hexadecimal/src/test/java/HexadecimalTest.java
+++ 220614-hexadecimal-jmj/Hexadecimal/src/test/java/HexadecimalTest.java
@@ -18,4 +18,15 @@ class HexadecimalTest {
     assertEquals("A", hexadecimal.compute(10));
     assertEquals("5C", hexadecimal.compute(92));
   }
+
+  @Test
+  void inputNumberOver100() {
+    Hexadecimal hexadecimal = new Hexadecimal();
+    assertEquals("80", hexadecimal.compute(128));
+    assertEquals("396", hexadecimal.compute(918));
+    assertEquals("404", hexadecimal.compute(1028));
+    assertEquals("131D", hexadecimal.compute(4893));
+    assertEquals("4B54", hexadecimal.compute(19284));
+    assertEquals("2CDC4", hexadecimal.compute(183748));
+  }
 }
