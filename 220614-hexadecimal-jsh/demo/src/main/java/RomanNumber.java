c4f4617e1b37f509ad50923f64acc36e97d66fef
sunghwan22 <tidls3144@naver.com>
2022-06-15 02:55:10 +0900

    2개의 문자를 받았을때 테스트를 통과하게 작성

--- 220614-hexadecimal-jsh/demo/src/main/java/RomanNumber.java
+++ 220614-hexadecimal-jsh/demo/src/main/java/RomanNumber.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RomanNumber {
+  private int arabiaNumber = 0;
+  private int arabiaNumberSum = 0;
   public void main(String[] args){
     Scanner scanner = new Scanner(System.in);
 
@@ -15,12 +19,36 @@ public class RomanNumber {
   }
 
   public int convert(String romanNumber) {
-    if(romanNumber.equals("I")){
-      return 1;
+    String[] romaNumberList = romanNumber.split("");
+
+    List<Integer> arabiaNumberList = new ArrayList<>();
+
+    for (int i = 0; i < romaNumberList.length; i += 1) {
+      if (romaNumberList[i].equals("I")) {
+        arabiaNumber = 1;
+      }
+      if (romaNumberList[i].equals("V")) {
+        arabiaNumber = 5;
+      }
+      if (romaNumberList[i].equals("X")) {
+        arabiaNumber = 10;
+      }
+      if (romaNumberList[i].equals("L")) {
+        arabiaNumber = 50;
+      }
+      if (romaNumberList[i].equals("C")) {
+        arabiaNumber = 100;
+      }
+      if (romaNumberList[i].equals("D")) {
+        arabiaNumber = 500;
+      }
+      if (romaNumberList[i].equals("M")) {
+        arabiaNumber = 1000;
+      }
+
+      arabiaNumberList.add(arabiaNumber);
+      arabiaNumberSum += arabiaNumber;
     }
-    if(romanNumber.equals("L")){
-      return 50;
-    }
-    return 0;
+    return arabiaNumberSum;
   }
 }
--- 220614-hexadecimal-jsh/demo/src/test/java/RomanNumberTest.java
+++ 220614-hexadecimal-jsh/demo/src/test/java/RomanNumberTest.java
@@ -11,5 +11,12 @@ class RomanNumberTest {
     assertEquals(1, romanNumber.convert("I"));
     assertEquals(50, romanNumber.convert("L"));
   }
+  @Test
+  void over2CharactersTest() {
+    RomanNumber romanNumber = new RomanNumber();
+
+    assertEquals(60, romanNumber.convert("LX"));
+    assertEquals(150, romanNumber.convert("CL"));
+  }
 
 }
