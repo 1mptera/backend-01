e58315709383c064dcee64c556532ca1d673a7a7
seungjjun <il1uso@naver.com>
2022-06-15 09:10:31 +0900

    준비 > 입력 > 처리 > 출력 각 구조 설계함

---
+++ 220615-romaNumber-nsj/RomaNumber/src/main/java/RomaNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class RomaNumber {
+  public static void main(String[] args) {
+    RomaNumber application = new RomaNumber();
+    application.run();
+  }
+
+  public void run() {
+    // 준비
+    Scanner scanner = new Scanner(System.in);
+
+    // 입력
+    System.out.print("로마 숫자 : ");
+    String romaNumber = scanner.nextLine();
+
+    // 처리
+    int arabiaNumber = process(romaNumber);
+
+    // 출력
+    System.out.print("아라비아 숫자 : " + arabiaNumber);
+  }
+
+  private int process(String romaNumber) {
+    return 0;
+  }
+}
+
