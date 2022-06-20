465eddb6a53732f61ef4ab9b223e6ec9fb294dd6
seungjjun <il1uso@naver.com>
2022-06-20 13:25:16 +0900

    사용자 요청을 받으면 Hello, World 메세지 전송하는 서버 구축

--- 220620-httpserver-nsj-hiw/HttpServer/src/main/java/MakaoBank.java
+++ 220620-httpserver-nsj-hiw/HttpServer/src/main/java/MakaoBank.java
@@ -1,3 +1,32 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class MakaoBank {
-  
+  public static void main(String[] args) throws IOException {
+    MakaoBank application = new MakaoBank();
+    application.run();
+  }
+
+  public void run() throws IOException {
+    HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+
+    httpServer.createContext("/", (exchange) -> {
+      String content = "Hello, World!";
+
+      exchange.sendResponseHeaders(200, content.getBytes().length);
+
+      OutputStream outputStream = exchange.getResponseBody();
+      outputStream.write(content.getBytes());
+      outputStream.flush();
+      outputStream.close();
+    });
+
+    httpServer.start();
+
+    System.out.println("Server is listening... http://localhost:8000");
+  }
+
 }
