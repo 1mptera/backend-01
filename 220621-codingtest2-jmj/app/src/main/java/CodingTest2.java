be2b443b519b49b40b80345abecb9b0b187ad7fd
Jeon Minji <mjjeon2645@gmail.com>
2022-06-21 10:01:10 +0900

    메인클래스의 비즈니스 로직 refactoring
    1. for문을 사용하여 a, b 배열의 각 인덱스별 값을 곱해준 뒤 answer에 결과값을 더해주는 방식으로 리팩터링
    2. 테스트 통과 완료

--- 220621-codingtest2-jmj/app/src/main/java/CodingTest2.java
+++ 220621-codingtest2-jmj/app/src/main/java/CodingTest2.java
@@ -2,6 +2,10 @@ public class CodingTest2 {
   public static int solution(int[] a, int[] b) {
     int answer = 1234567890;
     answer = 0;
+
+    for (int i = 0; i < a.length; i += 1) {
+      answer += a[i] * b[i];
+    }
     return answer;
   }
 }
--- 220621-codingtest2-jmj/app/src/test/java/CodingTest2Test.java
+++ 220621-codingtest2-jmj/app/src/test/java/CodingTest2Test.java
@@ -11,4 +11,22 @@ class CodingTest2Test {
 
     assertEquals(0, CodingTest2.solution(a, b));
   }
+
+  @Test
+  void exampleNumber1() {
+    CodingTest2 codingTest2 = new CodingTest2();
+    int[] a = new int[]{1, 2, 3, 4};
+    int[] b = new int[]{-3, -1, 0, 2};
+
+    assertEquals(3, CodingTest2.solution(a, b));
+  }
+
+  @Test
+  void exampleNumber2() {
+    CodingTest2 codingTest2 = new CodingTest2();
+    int[] a = new int[]{-1, 0, 1};
+    int[] b = new int[]{1, 0, -1};
+
+    assertEquals(-2, CodingTest2.solution(a, b));
+  }
 }
