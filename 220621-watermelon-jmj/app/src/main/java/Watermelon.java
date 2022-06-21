0c0cae6b9ad49aeb6221d788d91bdb053216b555
Jeon Minji <mjjeon2645@gmail.com>
2022-06-21 09:43:45 +0900

    메인클래스 리팩터링 후 test 통과 완료
    1. 경우의 수를 홀수/짝수입력 두가지로 나눔
    2. 짝수 입력 시 해당 수를 2로 나눈 만큼 "수박"이라는 문구를 answer에 반복 누적
    3. 홀수 입력 시 해당 수를 2로 나눈 만큼 "수박"이라는 문구를 aswer에 반복 누적시킨 뒤 맨 마지막에 "수"를 한번 더 누적시키고 리턴

--- 220621-watermelon-jmj/app/src/main/java/Watermelon.java
+++ 220621-watermelon-jmj/app/src/main/java/Watermelon.java
@@ -1,8 +1,17 @@
 public class Watermelon {
   public String solution(int n) {
     String answer = "";
-    if (n == 3) {
-      answer = "수박수";
+
+    if (n % 2 == 0) {
+      for (int i = 0; i < n / 2; i += 1) {
+        answer += "수박";
+      }
+    }
+    if (n % 2 != 0) {
+      for (int i = 0; i < n / 2; i += 1) {
+        answer += "수박";
+      }
+      answer += "수";
     }
     return answer;
   }
--- 220621-watermelon-jmj/app/src/test/java/WatermelonTest.java
+++ 220621-watermelon-jmj/app/src/test/java/WatermelonTest.java
@@ -15,5 +15,14 @@ class WatermelonTest {
     Watermelon watermelon = new Watermelon();
 
     assertEquals("수박수", watermelon.solution(3));
+    assertEquals("수박수박", watermelon.solution(4));
+  }
+
+  @Test
+  void inputRandomNumber() {
+    Watermelon watermelon = new Watermelon();
+
+    assertEquals("수박수박수박수", watermelon.solution(7));
+    assertEquals("수박수박수박수박수박", watermelon.solution(10));
   }
 }
