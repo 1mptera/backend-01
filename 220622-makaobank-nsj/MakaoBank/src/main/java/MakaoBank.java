138bae8fe212904b11b8c6cf86399b38ca3e70bd
seungjjun <il1uso@naver.com>
2022-06-22 22:56:01 +0900

    makaobank 6강 인출 연습

--- 220622-makaobank-nsj/MakaoBank/src/main/java/MakaoBank.java
+++ 220622-makaobank-nsj/MakaoBank/src/main/java/MakaoBank.java
@@ -7,23 +7,33 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 public class MakaoBank {
-  Account account;
-  private TransferService transferService;
+  private final FormParser formParser;
+
+  private final Account account;
+  private final TransferService transferService;
+  private final List<Account> accounts;
 
   public static void main(String[] args) throws IOException {
     MakaoBank application = new MakaoBank();
     application.run();
   }
 
-  public void run() throws IOException {
-    List<Account> accounts = List.of(
+  public MakaoBank() {
+    formParser = new FormParser();
+
+    accounts = List.of(
         new Account("1234", "ashal", 3000),
         new Account("2345", "joker", 1000)
     );
     account = accounts.get(0);
     transferService = new TransferService(accounts);
+  }
+
+  public void run() throws IOException {
+
 
     InetSocketAddress address = new InetSocketAddress(8000);
     HttpServer httpServer = HttpServer.create(address, 0);
@@ -35,14 +45,15 @@ public class MakaoBank {
 
       String method = exchange.getRequestMethod();
 
-      // 처리
-      PageGenerator pageGenerator = process(path, method);
+      String requestBody = new RequestBodyReader(exchange).body();
 
-      String content = pageGenerator.html();
+      Map<String, String> formData = formParser.parse(requestBody);
+      
+      // 처리
+      PageGenerator pageGenerator = process(path, method, formData);
 
       // 출력
-      MessageWriter messageWriter = new MessageWriter(exchange);
-      messageWriter.write(content);
+      new MessageWriter(exchange).write(pageGenerator.html());
     });
 
     httpServer.start();
@@ -50,32 +61,41 @@ public class MakaoBank {
     System.out.print("http://localhost:8000/");
   }
 
-  private PageGenerator process(String path, String method) {
-    return switch (path) {
-      case "/account" -> processAccount();
-      case "/transfer" -> processTransfer(method);
+  private PageGenerator process(String path, String method,
+                                Map<String, String> formData) {
+    String[] steps = path.substring(1).split("/");
+    return switch (steps[0]) {
+      case "account" -> processAccount(steps.length > 1 ? steps[1] : "");
+      case "transfer" -> processTransfer(method, formData);
       default -> new GreetingPageGenerator();
     };
   }
 
-  private AccountPageGenerator processAccount() {
-    return new AccountPageGenerator(account);
+  private AccountPageGenerator processAccount(String identifier) {
+    Account found = accounts.stream()
+        .filter(account -> account.identifier().equals(identifier))
+        .findFirst()
+        .orElse(account);
+    return new AccountPageGenerator(found);
   }
 
-  private PageGenerator processTransfer(String method) {
+  private PageGenerator processTransfer(String method, Map<String, String> formData) {
     if (method.equals("GET")) {
       return processTransferGet();
     }
-    return processTransferPost();
+    return processTransferPost(formData);
   }
 
   private TransferPageGenerator processTransferGet() {
     return new TransferPageGenerator(account);
   }
 
-  private TransferSuccessPageGenerator processTransferPost() {
+  private TransferSuccessPageGenerator processTransferPost(Map<String, String> formData) {
     // TODO : 진짜 처리
-    transferService.transfer("1234", "2345", 1000);
+    transferService.transfer(
+        account.identifier(),
+        formData.get("to"),
+        Long.parseLong(formData.get("amount")));
 
     return new TransferSuccessPageGenerator(account);
   }
