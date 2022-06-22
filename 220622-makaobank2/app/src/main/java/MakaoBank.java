fdc3ea7f6f54cf3adcc6f1e20ef0065e2ca3a901
Jeon Minji <mjjeon2645@gmail.com>
2022-06-22 17:30:37 +0900

    4강까지 인출학습 완료

--- 220622-makaobank2/app/src/main/java/MakaoBank.java
+++ 220622-makaobank2/app/src/main/java/MakaoBank.java
@@ -1,16 +1,13 @@
 // 1. http server 생성과 본인 이름 path로 지정하여 페이지 문구 출력하게 하기
 // 2. 마카오뱅크 기능 구현. 주요 기능 3가지
 //  1) account. 계좌 생성, html로 텍스트 뿌려주기, PageGen 생성
-//  2) transfer
+//  2) transfer transferPage 생성,
 //  3) transactions
 
 
 import com.sun.net.httpserver.HttpServer;
 import models.Account;
-import utils.AccountPageGenerator;
-import utils.GreetingPageGenerator;
-import utils.PageGenerator;
-import utils.MessageWriter;
+import utils.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -32,13 +29,20 @@ public class MakaoBank {
       URI requestURI = exchange.getRequestURI();
       String path = requestURI.getPath();
 
-      // 처리
-      PageGenerator pageGenerator = new GreetingPageGenerator();
+      String method = exchange.getRequestMethod();
 
-      if (path.equals("/account")) {
-        Account account = new Account("1234", "Ashal", 3000);
-        pageGenerator = new AccountPageGenerator(account);
-      }
+      // 처리
+      Account account = new Account("1234", "Ashal", 3000);
+
+
+        PageGenerator pageGenerator = switch (path) {
+          case "/account" -> new AccountPageGenerator(account);
+          case "/transfer" -> method.equals("GET")
+          ? new TransferPageGenerator(account)
+          : new TransferProcessPageGenerator(account);
+          default -> new GreetingPageGenerator();
+        };
+
 
       String content = pageGenerator.html();
 
---
+++ 220622-makaobank2/app/src/test/java/utils/TransferProcessPageGeneratorTest.java
@@ -0,0 +1,9 @@
+package utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransferProcessPageGeneratorTest {
+
+  //TODO: 제대로 된 코드 짜 줄 것
+
+}
